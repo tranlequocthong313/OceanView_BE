61c6b475a8e8d53a003d39aa94cd2af21e74fb15
tranlequocthong313 <tranlequocthong313@gmail.com>
2024-04-11 17:35:59 +0700

    fix: change cache backend to database
    Pythonanywhere does not support a protocol for connecting to the redis

--- invoice/migrations/0001_initial.py
+++ invoice/migrations/0001_initial.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -16,32 +15,107 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='InvoiceType',
+            name="InvoiceType",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_date', models.DateTimeField(auto_now_add=True, verbose_name='Ngày tạo')),
-                ('updated_date', models.DateTimeField(auto_now=True, verbose_name='Ngày cập nhật')),
-                ('name', models.CharField(max_length=50, verbose_name='Tên loại hóa đơn')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created_date",
+                    models.DateTimeField(auto_now_add=True, verbose_name="Ngày tạo"),
+                ),
+                (
+                    "updated_date",
+                    models.DateTimeField(auto_now=True, verbose_name="Ngày cập nhật"),
+                ),
+                (
+                    "name",
+                    models.CharField(max_length=50, verbose_name="Tên loại hóa đơn"),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
         migrations.CreateModel(
-            name='Invoice',
+            name="Invoice",
             fields=[
-                ('created_date', models.DateTimeField(auto_now_add=True, verbose_name='Ngày tạo')),
-                ('updated_date', models.DateTimeField(auto_now=True, verbose_name='Ngày cập nhật')),
-                ('id', models.CharField(max_length=10, primary_key=True, serialize=False, verbose_name='Mã hóa đơn')),
-                ('payment_method', models.CharField(choices=[('E', 'Ví điện tử'), ('A', 'Ủy nhiệm chi')], max_length=1, verbose_name='Phương thức thanh toán')),
-                ('total', models.DecimalField(decimal_places=2, max_digits=11, validators=[django.core.validators.MinLengthValidator(0)], verbose_name='Tổng cộng')),
-                ('paid', models.DecimalField(decimal_places=2, max_digits=11, validators=[django.core.validators.MinLengthValidator(0)], verbose_name='Đã trả')),
-                ('trading_code', models.CharField(max_length=50, null=True, verbose_name='Mã giao dịch')),
-                ('payer', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, verbose_name='Giao dịch bởi')),
-                ('invoice_type', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='invoice.invoicetype', verbose_name='Loại hóa đơn')),
+                (
+                    "created_date",
+                    models.DateTimeField(auto_now_add=True, verbose_name="Ngày tạo"),
+                ),
+                (
+                    "updated_date",
+                    models.DateTimeField(auto_now=True, verbose_name="Ngày cập nhật"),
+                ),
+                (
+                    "id",
+                    models.CharField(
+                        max_length=10,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="Mã hóa đơn",
+                    ),
+                ),
+                (
+                    "payment_method",
+                    models.CharField(
+                        choices=[("E", "Ví điện tử"), ("A", "Ủy nhiệm chi")],
+                        max_length=1,
+                        verbose_name="Phương thức thanh toán",
+                    ),
+                ),
+                (
+                    "total",
+                    models.DecimalField(
+                        decimal_places=2,
+                        max_digits=11,
+                        validators=[django.core.validators.MinLengthValidator(0)],
+                        verbose_name="Tổng cộng",
+                    ),
+                ),
+                (
+                    "paid",
+                    models.DecimalField(
+                        decimal_places=2,
+                        max_digits=11,
+                        validators=[django.core.validators.MinLengthValidator(0)],
+                        verbose_name="Đã trả",
+                    ),
+                ),
+                (
+                    "trading_code",
+                    models.CharField(
+                        max_length=50, null=True, verbose_name="Mã giao dịch"
+                    ),
+                ),
+                (
+                    "payer",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="Giao dịch bởi",
+                    ),
+                ),
+                (
+                    "invoice_type",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to="invoice.invoicetype",
+                        verbose_name="Loại hóa đơn",
+                    ),
+                ),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
