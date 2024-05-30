c87a49f1d32fc3d5b4b179f93772b3323271e9ef
tranlequocthong313 <tranlequocthong313@gmail.com>
2024-05-30 15:26:14 +0700

    fix: fcm

--- firebase/service_worker.py
+++ firebase/service_worker.py
@@ -1,10 +1,9 @@
-# TODO: I have no clue how to serve this from a JS file
-# TODO: PLS FIND A WAY TO MOVE THIS OUT OF HERE :(
 from django.http import HttpResponse
 
 
+# TODO: I have no clue how to serve this from a JS file
+# TODO: PLS FIND A WAY TO MOVE THIS OUT OF HERE :(
 def service_worker(request):
-    # WARNING: LNGTM!!
     data = (
         'importScripts("https://www.gstatic.com/firebasejs/9.18.0/firebase-app-compat.js");'
         'importScripts("https://www.gstatic.com/firebasejs/9.18.0/firebase-messaging-compat.js"); '
--- utils/firebase.py
+++
@@ -1,4 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-firebase_admin.initialize_app()
